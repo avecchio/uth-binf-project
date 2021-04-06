a33ae3ba739cca7004fe95e8f7186942693bbccf
Austin Vecchio <au.vecchio@gmail.com>
2021-04-06 10:04:03 -0600

    Working lcs

--- topological.py
+++ topological.py
@@ -693,7 +693,73 @@ from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 from skbio.alignment import StripedSmithWaterman
 
+
+def LCSSubStr(X: str, Y: str,
+                   m: int, n: int):
+    # Create a table to store lengths of
+    # longest common suffixes of substrings.
+    # Note that LCSuff[i][j] contains length
+    # of longest common suffix of X[0..i-1] and
+    # Y[0..j-1]. The first row and first
+    # column entries have no logical meaning,
+    # they are used only for simplicity of program
+    LCSuff = [[0 for i in range(n + 1)]
+                 for j in range(m + 1)]
+ 
+    # To store length of the
+    # longest common substring
+    length = 0
+ 
+    # To store the index of the cell
+    # which contains the maximum value.
+    # This cell's index helps in building
+    # up the longest common substring
+    # from right to left.
+    row, col = 0, 0
+ 
+    # Following steps build LCSuff[m+1][n+1]
+    # in bottom up fashion.
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                LCSuff[i][j] = 0
+            elif X[i - 1] == Y[j - 1]:
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1
+                if length < LCSuff[i][j]:
+                    length = LCSuff[i][j]
+                    row = i
+                    col = j
+            else:
+                LCSuff[i][j] = 0
+ 
+    # if true, then no common substring exists
+    if length == 0:
+        print("No Common Substring")
+        return
+ 
+    # allocate space for the longest
+    # common substring
+    resultStr = ['0'] * length
+ 
+    # traverse up diagonally form the
+    # (row, col) cell until LCSuff[row][col] != 0
+    while LCSuff[row][col] != 0:
+        length -= 1
+        resultStr[length] = X[row - 1] # or Y[col-1]
+ 
+        # move diagonally up to previous cell
+        row -= 1
+        col -= 1
+ 
+    # required longest common substring
+    return ''.join(resultStr)
+ 
+  
+
 def generate_circular_rna_structural_variants(circular_rnas, chromosome, xonic_sequences, circ_rna_sequences):
+
+    actual_count = 0
+    impacted_length = 0
 
     #rna_sequences = read_fasta('asdf.fasta')
     for circular_rna in circular_rnas:
@@ -714,8 +780,25 @@ def generate_circular_rna_structural_variants(circular_rnas, chromosome, xonic_s
             genomic_seq = get_sequence_from_ensembl(chromosome, rna_start + 1, rna_end)
             dna_string = ''.join(genomic_seq.split("\n")[1:])
             real_rna = circ_rna_sequences[identifier]
-            if real_rna[0:15] in dna_string:
-                print('yiss')
+
+            processing = True
+            while processing:
+                X = dna_string
+                Y = real_rna
+                m = len(X)
+                n = len(Y)
+
+                sub_string = LCSSubStr(X, Y, m, n)
+                if sub_string == None:
+                    processing = False
+                else:
+                    print(len(sub_string))
+                    real_rna = real_rna.replace(sub_string, "-")
+
+            
+            
+            #if real_rna[0:15] in dna_string:
+            #    print('yiss')
 
             #query = StripedSmithWaterman(dna_string)
             #alignment = query(real_rna)
